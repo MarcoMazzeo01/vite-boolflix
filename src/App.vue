c0cfec95bcc38dbfd06633f520b16070ab953d66
plastikSun <marcom2k01@gmail.com>
2023-09-15 20:28:37 +0200

    add: search bar, research API (shows only result of i = 0)

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,48 @@
-<script></script>
-<template></template>
-<style lang="scss">
+<script>
+const API_Key = "b933e4ab38909d92d09a5a2221ccd525";
+const API_URL = "https://api.themoviedb.org/3/search/movie?query=";
+
+//components
+import Header from "./components/Header.vue";
+
+//extras
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      result: undefined,
+    };
+  },
+
+  components: { Header },
+
+  methods: {
+    searchQuery(query) {
+      console.log("Searching: " + query);
+      if (query.length > 0 && typeof query == "string") {
+        axios
+          .get(API_URL + query + "&language=it-IT" + "&api_key=" + API_Key)
+          .then((resp) => {
+            const results = resp.data.results[0];
+            console.log(results);
+            this.result = results;
+
+            const { title, original_title, original_language, vote_average } =
+              results;
+            console.log(title, original_title, original_language, vote_average);
+          });
+      } else {
+        console.log("You can't input an empty query.");
+      }
+    },
+  },
+};
+</script>
+<template>
+  <Header @onForwardQuery="searchQuery" />
+  {{ result }}
+</template>
+<style lang="scss" scoped>
 @use "./assets/scss/style.scss" as *;
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+
+  emits: ["onForwardQuery"],
+};
+</script>
+
+<template>
+  <div class="input-group">
+    <input
+      type="search"
+      class="form-control rounded"
+      placeholder="Search"
+      aria-label="Search"
+      aria-describedby="search-addon"
+      v-model.lazy="searchQuery"
+      @keyup.enter="$emit('onForwardQuery', searchQuery)"
+    />
+    <button
+      type="button"
+      class="btn btn-outline-primary"
+      @click="$emit('onForwardQuery', searchQuery)"
+    >
+      Search
+    </button>
+  </div>
+
+  {{ searchQuery }}
+</template>
+
+<style scoped></style>
