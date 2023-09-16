04eb5d681b60d7ed60ade620a9128cc734c7b9a4
plastikSun <marcom2k01@gmail.com>
2023-09-16 14:59:38 +0200

    add: tv series search \ complete: milestone 2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 const API_Key = "b933e4ab38909d92d09a5a2221ccd525";
-const API_URL = "https://api.themoviedb.org/3/search/movie?query=";
+const moviesAPI = "https://api.themoviedb.org/3/search/movie?query=";
+const seriesAPI = "https://api.themoviedb.org/3/search/tv?query=";
 
 //components
 import Header from "./components/Header.vue";
@@ -25,8 +26,9 @@ export default {
       console.log("Searching: " + query);
 
       if (query.length > 0 && typeof query == "string") {
+        //movies request
         axios
-          .get(API_URL + query + "&language=it-IT" + "&api_key=" + API_Key)
+          .get(moviesAPI + query + "&language=it-IT" + "&api_key=" + API_Key)
           .then((resp) => {
             const results = resp.data.results;
             console.table(results);
@@ -53,6 +55,36 @@ export default {
 
             store.movies = resultsArray;
           });
+
+        //series request
+        axios
+          .get(seriesAPI + query + "&language=it-IT" + "&api_key=" + API_Key)
+          .then((resp) => {
+            const results = resp.data.results;
+            console.table(results);
+            const resultsArray = [];
+            results.forEach((show) => {
+              const {
+                name,
+                original_name,
+                original_language,
+                vote_average,
+                first_air_date,
+              } = show;
+
+              const showObj = {
+                title: name,
+                original_title: original_name,
+                original_language: original_language,
+                vote_average: vote_average,
+                release: first_air_date,
+              };
+
+              resultsArray.push(showObj);
+            });
+
+            store.series = resultsArray;
+          });
       } else {
         console.log("You can't input an empty query.");
       }
@@ -66,8 +98,8 @@ export default {
 </script>
 <template>
   <Header @onForwardQuery="searchQuery" />
-  {{ result }}
 
+  <h2>Movies</h2>
   <ol>
     <li v-for="movie in store.movies" class="bg-primary">
       <h3>{{ movie.title }} {{ displayRelease(movie.release) }}</h3>
@@ -86,6 +118,28 @@ export default {
         style="width: 20px"
       />
       <p>Vote: {{ movie.vote_average }}</p>
+    </li>
+  </ol>
+
+  <h2>Series</h2>
+  <ol>
+    <li v-for="show in store.series" class="bg-warning">
+      <h3>{{ show.title }} {{ displayRelease(show.release) }}</h3>
+      <h5>{{ show.original_title }}</h5>
+      <img
+        v-if="this.langs.includes(show.original_language)"
+        :src="'/flags/' + show.original_language + '.svg'"
+        :alt="show.original_language"
+        style="width: 20px"
+      />
+
+      <img
+        v-else
+        src="/flags/unknown.svg"
+        :alt="show.original_language"
+        style="width: 20px"
+      />
+      <p>Vote: {{ show.vote_average }}</p>
     </li>
   </ol>
 </template>
