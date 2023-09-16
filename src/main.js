e6764fc6e956a2ad03116644e55ab26ff480234c
plastikSun <marcom2k01@gmail.com>
2023-09-16 22:37:47 +0200

    install: fontawesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as bootstrap from "bootstrap";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(fas);
+
 import { createApp } from "vue";
 import App from "./App.vue";
 
 const Boolflix = createApp(App);
+Boolflix.component("font-awesome-icon", FontAwesomeIcon);
 Boolflix.mount("#app");
