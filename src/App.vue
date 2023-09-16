95fd6568b89815db32fbd2d8076f88ef266ea74f
plastikSun <marcom2k01@gmail.com>
2023-09-16 15:48:47 +0200

    fix: made movie\series cards its own component

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,22 @@ const API_Key = "b933e4ab38909d92d09a5a2221ccd525";
 const moviesAPI = "https://api.themoviedb.org/3/search/movie?query=";
 const seriesAPI = "https://api.themoviedb.org/3/search/tv?query=";
 
-//components
-import Header from "./components/Header.vue";
-
 //extras
 import axios from "axios";
 import store from "./store.js";
 
+//components
+import Header from "./components/Header.vue";
+import Cards from "./components/Cards.vue";
+
 export default {
   data() {
     return {
-      result: undefined,
       store,
-      langs: ["en", "es", "fr", "it", "ja", "ko"],
     };
   },
 
-  components: { Header },
+  components: { Header, Cards },
 
   methods: {
     searchQuery(query) {
@@ -53,7 +52,7 @@ export default {
               resultsArray.push(movie);
             });
 
-            store.movies = resultsArray;
+            this.store.movies = resultsArray;
           });
 
         //series request
@@ -83,65 +82,18 @@ export default {
               resultsArray.push(showObj);
             });
 
-            store.series = resultsArray;
+            this.store.series = resultsArray;
           });
       } else {
         console.log("You can't input an empty query.");
       }
-    },
-
-    displayRelease(date) {
-      return date != "" ? "(" + date.split("-")[0] + ")" : "";
     },
   },
 };
 </script>
 <template>
   <Header @onForwardQuery="searchQuery" />
-
-  <h2>Movies</h2>
-  <ol>
-    <li v-for="movie in store.movies" class="bg-primary">
-      <h3>{{ movie.title }} {{ displayRelease(movie.release) }}</h3>
-      <h5>{{ movie.original_title }}</h5>
-      <img
-        v-if="this.langs.includes(movie.original_language)"
-        :src="'/flags/' + movie.original_language + '.svg'"
-        :alt="movie.original_language"
-        style="width: 20px"
-      />
-
-      <img
-        v-else
-        src="/flags/unknown.svg"
-        :alt="movie.original_language"
-        style="width: 20px"
-      />
-      <p>Vote: {{ movie.vote_average }}</p>
-    </li>
-  </ol>
-
-  <h2>Series</h2>
-  <ol>
-    <li v-for="show in store.series" class="bg-warning">
-      <h3>{{ show.title }} {{ displayRelease(show.release) }}</h3>
-      <h5>{{ show.original_title }}</h5>
-      <img
-        v-if="this.langs.includes(show.original_language)"
-        :src="'/flags/' + show.original_language + '.svg'"
-        :alt="show.original_language"
-        style="width: 20px"
-      />
-
-      <img
-        v-else
-        src="/flags/unknown.svg"
-        :alt="show.original_language"
-        style="width: 20px"
-      />
-      <p>Vote: {{ show.vote_average }}</p>
-    </li>
-  </ol>
+  <Cards />
 </template>
 <style lang="scss" scoped>
 @use "./assets/scss/style.scss" as *;
---
+++ src/components/Cards.vue
@@ -0,0 +1,66 @@
+<script>
+import store from "../store.js";
+
+export default {
+  data() {
+    return {
+      store,
+      langs: ["en", "es", "fr", "it", "ja", "ko"],
+    };
+  },
+
+  methods: {
+    displayReleaseYear(date) {
+      return date != "" ? "(" + date.split("-")[0] + ")" : "";
+    },
+  },
+};
+</script>
+
+<template>
+  <h2>Movies</h2>
+  <ol>
+    <li v-for="movie in store.movies" class="bg-primary">
+      <h3>{{ movie.title }} {{ displayReleaseYear(movie.release) }}</h3>
+      <h5>{{ movie.original_title }}</h5>
+      <img
+        v-if="this.langs.includes(movie.original_language)"
+        :src="'/flags/' + movie.original_language + '.svg'"
+        :alt="movie.original_language"
+        style="width: 20px"
+      />
+
+      <img
+        v-else
+        src="/flags/unknown.svg"
+        :alt="movie.original_language"
+        style="width: 20px"
+      />
+      <p>Vote: {{ movie.vote_average }}</p>
+    </li>
+  </ol>
+
+  <h2>Series</h2>
+  <ol>
+    <li v-for="show in store.series" class="bg-warning">
+      <h3>{{ show.title }} {{ displayReleaseYear(show.release) }}</h3>
+      <h5>{{ show.original_title }}</h5>
+      <img
+        v-if="this.langs.includes(show.original_language)"
+        :src="'/flags/' + show.original_language + '.svg'"
+        :alt="show.original_language"
+        style="width: 20px"
+      />
+
+      <img
+        v-else
+        src="/flags/unknown.svg"
+        :alt="show.original_language"
+        style="width: 20px"
+      />
+      <p>Vote: {{ show.vote_average }}</p>
+    </li>
+  </ol>
+</template>
+
+<style lang="scss" scoped></style>
